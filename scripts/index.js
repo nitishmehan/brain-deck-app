9f4bae9911717466d9fe3c6365cdeec85b5a2e97
Nitish <nitishmehan0608@gmail.com>
2025-10-17 00:38:25 +0530

    updates on home page

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,7 @@ let size = cards.length;
 
 function next() {
     let prev = index;
+    cards[prev].classList.remove('flip');
     index = (index+1)%size;
     cards.forEach((card,i)=>{
         if(i==prev) {card.classList.add('card-hidden'); card.classList.remove('card-active');}
@@ -13,15 +14,14 @@ function next() {
 
 function previous() {
     let prev = index;
+    cards[prev].classList.remove('flip');
     index = (index - 1 + size) % size;
-    cards.forEach((card,i)=>{
-        if(i==prev) {card.classList.add('card-hidden'); card.classList.remove('card-active');}
-        if(i==index) {card.classList.remove('card-hidden'); card.classList.add('card-active')}
-    })
+    cards[prev].classList.add('card-hidden'); cards[prev].classList.remove('card-active');
+    cards[index].classList.remove('card-hidden'); cards[index].classList.add('card-active')
 }
 
 function flip() {
-
+    cards[index].classList.toggle('flip');
 }
 
 document.addEventListener('keydown',(event)=>{
